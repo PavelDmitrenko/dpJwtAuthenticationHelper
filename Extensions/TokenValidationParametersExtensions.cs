701b6dd0fa82d6cb80f8baa4ac72b8d4f3db9c48
PavelDmitrenko <dpv925@gmail.com>
2018-04-29 09:29:37 +0300

    Initial

---
+++ Extensions/TokenValidationParametersExtensions.cs
@@ -0,0 +1,17 @@
+﻿using dpJwtAuthenticationHelper.Types;
+using Microsoft.IdentityModel.Tokens;
+
+namespace dpJwtAuthenticationHelper.Extensions
+{
+    public static class TokenValidationParametersExtensions
+    {
+        public static TokenOptions ToTokenOptions(this TokenValidationParameters tokenValidationParameters,
+            int tokenExpiryInMinutes = 5)
+        {
+            return new TokenOptions(tokenValidationParameters.ValidIssuer,
+                tokenValidationParameters.ValidAudience,
+                tokenValidationParameters.IssuerSigningKey,
+                tokenExpiryInMinutes);
+        }
+    }
+}
